a50527d959b8135b426d793411fc659d69b2f579
StevenvL <stevenluu4x4@gmail.com>
2019-02-01 20:56:06 -0800

    Finish squaresOfASortedArray

--- src/squaresOfASortedArray/Solution.java
+++ src/squaresOfASortedArray/Solution.java
@@ -1,7 +1,20 @@
 package squaresOfASortedArray;
 
+import java.util.*;
+
 public class Solution {
-public int[] sortedSquares(int[] A) {
-        
-    }
+	public int[] sortedSquares(int[] A) {
+		for (int i = 0; i < A.length; i++) {
+			A[i] = A[i] *A[i];
+		}
+		Arrays.sort(A);
+		return A;
+	}
+
+	public static void main(String args[]) {
+		Solution tester = new Solution();
+		int[] data = { -4, -1, 0, 3, 10 };
+		data = tester.sortedSquares(data);
+		System.out.println(Arrays.toString(data));
+	}
 }
