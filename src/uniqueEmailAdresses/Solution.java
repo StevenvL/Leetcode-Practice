2f78ba81bc7515f90fbd4f482e4f3f30b66e6fcd
StevenvL <stevenluu4x4@gmail.com>
2019-02-01 16:23:18 -0800

    First commit

---
+++ src/uniqueEmailAdresses/Solution.java
@@ -0,0 +1,37 @@
+package uniqueEmailAdresses;
+import java.util.*;
+
+public class Solution {
+	public int numUniqueEmails(String[] emails) {
+		Set<String> distinctEmails = new TreeSet<>();
+		
+		for (int i = 0; i < emails.length; i++) {
+			String currentEmail = emails[i];
+			String[] localAndDomain = currentEmail.split("@");
+			String local = localAndDomain[0];
+			String domain = localAndDomain[1];
+			//System.out.println(localAndDomain[1]);
+			
+			if(local.contains("+")) {
+				int upTo = local.indexOf("+");
+				local = local.substring(0,upTo);
+			}
+			
+			if(local.contains(".")) {
+				String temp = "";
+				local = local.replace(".", "");
+			}
+			
+			String email = local + "@" + domain;
+			System.out.println(email);
+			distinctEmails.add(email);
+		}
+		return distinctEmails.size();
+	}
+	
+	public static void main (String args[]) {
+		Solution tester = new Solution();
+		String [] emails = {"test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"};
+		System.out.println(tester.numUniqueEmails(emails));
+	}
+}
