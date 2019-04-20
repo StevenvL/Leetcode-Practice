4dc91d2c1bc47f8fbea4289eb1a1568595a2bc66
StevenvL <stevenluu4x4@gmail.com>
2019-04-19 19:13:20 -0700

    Finish BetterCompression, working out solving it another way as well.

---
+++ src/LinkedInPractice/BetterCompression.java
@@ -0,0 +1,49 @@
+package LinkedInPractice;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+/**
+ * @author stevenluu
+ * Uses a Hashmap to solve the BetterCompression Problem
+ */
+public class BetterCompression {
+	public static void main(String[] args) {
+		String s = "a12c56a1b5";
+		System.out.println(betterCompression(s));
+	}
+	
+	public static String iterateHmap(HashMap<Character, Integer> hmapToPrint) {
+		String result = "";
+		for (Entry<Character, Integer> entry : hmapToPrint.entrySet()) {
+			Character key = entry.getKey();
+			Integer value = entry.getValue();
+			result = result + key + value;
+		}
+		return result;
+	}
+
+	public static String betterCompression(String s) {
+		HashMap<Character, Integer> hmap = new HashMap<>();
+
+		for (int i = 0; i < s.length(); i++) {
+			if (Character.isLetter(s.charAt(i))) {
+				String currentValue = "";
+				int isNumberIndex = i + 1;
+
+				while (isNumberIndex + 1 <= s.length() && Character.isDigit(s.charAt(isNumberIndex))) {
+					currentValue += s.charAt(isNumberIndex);
+					isNumberIndex++;
+				}
+
+				if (hmap.containsKey(s.charAt(i))) {
+					int valueOfCurrentLetterInHMap = hmap.get(s.charAt(i));
+					int total = valueOfCurrentLetterInHMap + Integer.parseInt(currentValue);
+					hmap.put(s.charAt(i), total);
+				} else
+					hmap.put(s.charAt(i), Integer.parseInt(currentValue));
+			}
+		}
+		return iterateHmap(hmap);
+	}
+}
