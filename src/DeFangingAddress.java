6f0b33d27c65880124ae9b4c2721aba301eee8f8
StevenvL <stevenluu4x4@gmail.com>
2019-07-08 20:37:19 -0700

    Update DefangingAddress

--- src/DeFangingAddress.java
+++ src/DeFangingAddress.java
@@ -1,6 +1,17 @@
 
 public class DeFangingAddress {
-	 public String defangIPaddr(String address) {
-	        address.replace(".", "[.]");
-	    }
+	public String defangIPaddr(String address) {
+		return address.replace(".", "[.]");
+	}
+
+	public String defanIpAddr2(String address) {
+		StringBuilder s = new StringBuilder("");
+		for (int i = 0; i < address.length(); i++) {
+			if (address.charAt(i) == '.')
+				s.append("[.]");
+			else
+				s.append(address.charAt(i));
+		}
+		return s.toString();
+	}
 }
