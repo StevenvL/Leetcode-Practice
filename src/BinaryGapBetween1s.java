75796614c4f5b85b05abe0c4fb097ca95674bfde
StevenvL <stevenluu4x4@gmail.com>
2019-07-14 15:06:28 -0700

    Update BinaryGapBetween1's with better solution

--- src/BinaryGapBetween1s.java
+++ src/BinaryGapBetween1s.java
@@ -6,14 +6,14 @@ public class BinaryGapBetween1s {
 		String n = Integer.toBinaryString(N);
 		System.out.println(n);
 		int largestGap = 0;
-		
-		for(int i = 0; i < n.length(); i++) {
-			if(n.charAt(i) == '1') {
+
+		for (int i = 0; i < n.length(); i++) {
+			if (n.charAt(i) == '1') {
 				int count = 0;
-				for(int j = i+1; j <n.length(); j++) {
+				for (int j = i + 1; j < n.length(); j++) {
 					count++;
-					if(n.charAt(j) == '1') {
-						if(count > largestGap) {
+					if (n.charAt(j) == '1') {
+						if (count > largestGap) {
 							largestGap = count;
 						}
 						count = 0;
@@ -21,15 +21,23 @@ public class BinaryGapBetween1s {
 				}
 			}
 		}
-	
+
 		return largestGap;
 	}
-	
-	
-	
-	
-	public static void main (String[] args) {
+
+	public static int binaryGap2(int N) { // basically check 1's as they come out.
+		int res = 0;
+		for (int d = -32; N > 0; N /= 2, d++) {
+			if (N % 2 == 1) {
+				res = Math.max(res, d);
+				d = 0;
+			}
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
 		int N = 22;
-		System.out.println(binaryGap(N));
+		System.out.println(binaryGap2(N));
 	}
 }
